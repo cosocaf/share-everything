a4b9f12cb6c4bbe37de234409c50578c96591df9
cosocaf <cosocaf@gmail.com>
2022-08-15 17:23:02 +0900

    特に理由のないコミットがGitHubを襲う...

--- api/server/src/app.ts
+++ api/server/src/app.ts
@@ -2,8 +2,11 @@ import express from "express";
 import helmet from "helmet";
 import { MongoClient } from "mongodb";
 
+import { v4 } from "uuid";
+
 const app = express();
 app.use(helmet());
+app.use(express.json());
 
 const client = new MongoClient(
   "mongodb://admin:admin@mongo/share-everything?authSource=admin",
@@ -17,6 +20,45 @@ try {
 }
 
 const db = client.db();
+
+const rooms = db.collection("rooms");
+
+app.post("/rooms", async (_, res) => {
+  // TODO: 認証機能とかあったほうがいいよね
+  const id = v4();
+
+  try {
+    await rooms.insertOne({ id, content: "" });
+    res.send({ status: "success", result: { id } });
+  } catch (e) {
+    if (e instanceof Error) {
+      res.send({ status: "error", reason: e.message });
+    } else {
+      res.send({ status: "error", reason: "Unknown Error." });
+    }
+  }
+});
+app.get("/rooms/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const room = await rooms.findOne({ id }, { content: 1 });
+    if (room) {
+      res.send({ status: "success", result: { content: room.content } });
+    } else {
+      res.send({ status: "error", reason: "Invalid ID." });
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      res.send({ status: "error", reason: e.message });
+    } else {
+      res.send({ status: "error", reason: "Unknown Error." });
+    }
+  }
+});
+app.put("/rooms/:id", async (req, res) => {
+  const id = req.params.id;
+});
 
 app.listen(8080, "0.0.0.0", () => {
   console.log("Listening on port 8080.");
